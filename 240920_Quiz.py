610808227b9bbf0fb8b8bd75122c8582f3ea1fb1
leeminyoung7 <163504495+leeminyoung7@users.noreply.github.com>
2024-09-21 20:03:38 +0900

    240920_Quiz
    24년 9월 20일 3주차 Quiz

---
+++ 240920_Quiz.py
@@ -0,0 +1,14 @@
+string = 'Hello, World!'
+a = string[:5]
+print(a)
+
+result = (7 + 3) ** 2
+print(result)
+
+A = [1, 2, 3, 4, 5]
+result = A[::-1]
+print(result)
+
+fruits = {'apple': 3, 'banana': 5, 'cherry': 2}
+banana_value = fruits['banana']
+print(banana_value)
